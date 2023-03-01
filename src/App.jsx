cb9857dcf54922617065a86618001bb61bcd0f89
Shaun <shaun@thenetninja.co.uk>
2023-03-01 18:20:54 +0000

    lesson-4

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,21 @@ import banner from './assets/banner.png';
 import Card from './components/Card';
 
 function App() {
-  console.log('hello, ninjas!')
 
   return (
-    <div>
+    <div class="container m-auto">
       <header>
         <h1>Ninja Merch</h1>
       </header>
       
-      <img src={banner} alt="site banner" />
+      <img class="rounded-md" src={banner} alt="site banner" />
       
-      <Card />
-      <Card />
-      <Card />
+      <div class="grid grid-cols-4 gap-10 my-4">
+        <Card />
+        <Card />
+        <Card />
+      </div>
+
     </div>
   );
 }
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,8 +1,9 @@
 export default function Card() {
   return (
-    <div>
+    <div class="bg-white p-4 text-center rounded-md shadow-md">
       <h2>Card Component</h2>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere, enim?</p>
+      <button class="btn">read more</button>
     </div>
   )
 }
