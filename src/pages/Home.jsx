3fe524be682e54414579dc88ddafdbbac5a26e5e
Shaun <shaun@thenetninja.co.uk>
2023-03-02 14:16:32 +0000

    lesson-10

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -12,20 +12,17 @@ export default function Home() {
   const [products] = createResource(fetchProducts)
 
   return (
-    <div class="grid grid-cols-4 gap-10 my-4">
-      <Card flat={true} rounded={false}>
-        <h2>Ninja Tee, black</h2>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid, expedita?</p>
-        <button class="btn">view</button>
-      </Card>
-      <Card flat={false} rounded={true}>
-        <h2>Ninja Tee, white</h2>
-        <button class="btn">view</button>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid, expedita?</p>
-        <p>Only £10</p>
-      </Card>
-
-      <p>{console.log(products(), products.loading)}</p>
-    </div>
+    <Show when={products()} fallback={<p>Loading...</p>}>
+      <div class="grid grid-cols-4 gap-10 my-4">
+        <For each={products()}>
+          {(product) => (
+            <Card rounded={true} flat={true}>
+              <img src={product.img} alt="product image" />
+              <h1 class="my-3 font-bold">{product.title}</h1>
+            </Card>
+          )}
+        </For>
+      </div>
+    </Show>
   )
 }
